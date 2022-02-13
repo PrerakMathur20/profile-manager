b13e7f9f20801b5ded458a9ff0b6a908f1a76a39
Prerak Mathur <mathur.prerak@gmail.com>
2022-02-13 08:19:06 +0530

    header fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="ui fixed menu">
       <div className="ui container center">
         <h2>Profile Manager</h2>&nbsp;by{" "}
-        <a href="gttps://github.com/PrerakMathur20">Prerak Mathur</a>
+        <a href="https://github.com/PrerakMathur20">Prerak Mathur</a>
 
         <div className="right menu">
           <div className="item">
